ae90db2b3c70ce13b8cad7b600ab48538fc34408
Josue2134 <josuehdez2134@gmail.com>
2024-04-17 08:38:53 -0600

    primer commit

---
+++ routes/Datos.routes.js
@@ -0,0 +1,13 @@
+import routerx from "express-promise-router";
+import Datosctr from "../controllers/Datos.controller";
+
+const router = routerx();
+
+router.post("/saveData", Datosctr.postDatos);
+router.get("/buscar", Datosctr.getDatos);
+router.get("/buscar/:id", Datosctr.getDato);
+router.patch("/actualizar/:id", Datosctr.putDatos);
+router.delete("/delete/:id",Datosctr.delDatos);
+
+
+export default router;
